29bdc5c121b8fea033b75bfea35b9e8961590279
Jiajie Chen <c@jia.je>
2024-09-22 00:29:07 +0800

    Show code

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -5,9 +5,10 @@ const width = props.code.length * 4 + (props.padding_left ? 2 : 0) + (props.padd
 </script>
 
 <template>
-  <svg :width="width" :height="210">
+  <svg :width="width" :height="220">
     <rect width="100%" height="100%" fill="white" />
     <line :x1="j * 4 + 2 + (padding_left ? 2 : 0)" y1="10" :x2="j * 4 + 2 + (padding_left ? 2 : 0)" :y2="actual_height" stroke-width="4" :stroke="parseInt(code[j]) ? `black` : `white`" v-for="j in Array(7).keys()" />
     <text :x="width / 2" :y="200" text-anchor="middle">{{ text }}</text>
+    <text :x="width / 2" :y="210" text-anchor="middle" font-size="5px">{{ code }}</text>
   </svg>
 </template>
